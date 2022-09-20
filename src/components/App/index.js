49348031ca9eece5e6c12f8881577f952194aae4
TomasConan <tomasconan.mail@gmail.com>
2022-09-20 14:42:27 +0200

    code(middleware): created action to connect with back

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,13 @@
 // == Import
+import { useDispatch } from 'react-redux';
+import { testLink } from '../../actions/test';
 import reactLogo from './react-logo.svg';
 import './styles.css';
 
 // == Composant
 function App() {
+  const dispatch = useDispatch();
+  dispatch(testLink());
   return (
     <div className="app">
       <img src={reactLogo} alt="react logo" />
