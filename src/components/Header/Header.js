40e91cbf91fda1c2959a095f9de0df75c011082b
Runebearer <guegan.l@gmail.com>
2022-09-20 15:50:26 +0200

    code(component): create header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+// import :
+import './styles.scss';
+
+// import components :
+
+// component :
+
+function Header() {
+  return (
+    <section>
+      <h1>FIND ME A REF</h1>
+      <h2>Solution d'arbitrage</h2>
+      <Navbar />
+      <Authenticator />
+      <img />
+    </section>
+  );
+}
+
+// export :
+export default Header;
