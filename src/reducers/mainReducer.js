40e0cb45beb558e46012a01d13c601a1dcf16bef
TomasConan <tomasconan.mail@gmail.com>
2022-09-20 14:20:00 +0200

    code(repository): create redux store

---
+++ src/reducers/mainReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  // ici le state initial
+};
+
+const mainReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default mainReducer;
