1040ccd3ebeff255fffdec11a7ec153ee0f37e87
Runebearer <guegan.l@gmail.com>
2022-09-21 14:50:03 +0200

    code(component): create commitment component

---
+++ src/components/Commitment/Commitment.js
@@ -0,0 +1,28 @@
+// import :
+import './styles.scss';
+
+// component :
+function Commitment() {
+  return (
+    <>
+      <section>
+        <p>DATE</p>
+        <h1>EQUIPE 1 VS EQUIPE 2</h1>
+        <p>Adresse</p>
+        <p>Catégorie</p>
+        <p>Type</p>
+        <p>Contact du match : Jean LeChef : 06 01 02 03 04</p>
+        <p>ARBITRES INSCRITS SUR CETTE RENCONTRE : 0/2</p>
+        <p>Prénom Arbitre 1</p>
+        <p>Prénom Arbitre 2</p>
+        <button type="button">J'arbitre !</button>
+      </section>
+      <section>
+        <p>MAP</p>
+      </section>
+    </>
+  );
+}
+
+// export :
+export default Commitment;
