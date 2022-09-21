bb68411fcf5cf1d766bc80f44bed4ebe7b3b3229
Runebearer <guegan.l@gmail.com>
2022-09-21 10:33:43 +0200

    code(component): create contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,28 @@
+// import :
+import './styles.scss';
+
+// component :
+function Contact() {
+  return (
+    <section>
+      <div>
+        <h1>Contact</h1>
+        <p>En cas de problème, merci de bien vouloir contacter l'administrateur du site :
+          Administrateur.du.site@lesite.fr
+        </p>
+      </div>
+      <div>
+        <h1>Notre Equipe :</h1>
+        <ul>
+          <li>Product Owner : Arnaud J</li>
+          <li>Scrum Master : Loïc G</li>
+          <li>Tech-Lead Back et GitMaster : Guillaume G</li>
+          <li>Tech-Lead Front : Tomas C</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+// export :
+export default Contact;
