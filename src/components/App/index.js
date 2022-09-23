2bb155f25e5f1f0463895e937fea00fb43e65168
Runebearer <guegan.l@gmail.com>
2022-09-23 16:26:56 +0200

    code(Header): wip integration

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,12 @@
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import Header from '../Header/Header';
+import './styles.scss';
 
 // == Composant
 function App() {
   return (
     <div className="app">
-      <img src={reactLogo} alt="react logo" />
-      <h1>Composant : App</h1>
+      <Header />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,20 @@
 // import :
 import './styles.scss';
+import whiteLogo from '../../assets/img/logo-ffrs-blanc.png';
 
 // import components :
 
 // component :
-
 function Header() {
   return (
-    <section>
-      <h1>FIND ME A REF</h1>
-      <h2>Solution d'arbitrage</h2>
-      <Navbar />
-      <Authenticator />
-      <img />
+    <section className="topPartHeader">
+      <div className="topPartHeader--headerTitle">
+        <h1>FIND ME A REF !</h1>
+        <h2>Solution d'arbitrage</h2>
+      </div>
+      {/* <Navbar />
+      <Login /> */}
+      <img className="topPartHeader--ffLogo" src={whiteLogo} alt="" />
     </section>
   );
 }
