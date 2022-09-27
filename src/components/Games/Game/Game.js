862339e5a14ad2eaf611eb9356bb98710be21e4c
Runebearer <guegan.l@gmail.com>
2022-09-27 10:58:44 +0200

    code(component): create and style games and game component

--- src/components/Games/Game/Game.js
+++ src/components/Games/Game/Game.js
@@ -4,31 +4,30 @@ import registering from 'src/assets/img/registering.png';
 
 // component :
 function Game() {
-  const gameState = 2;
+  let gameState = 'game';
+  const gameReferee = 0;
 
-  const gamecolorOne = 'game__noregistered';
-  const gameColorTwo = 'game__oneregistered';
-  const gameColorThree = 'game__fullregistered';
+  const gamecolorOne = ' noregistered';
+  const gameColorTwo = ' oneregistered';
+  const gameColorThree = ' fullregistered';
 
-  let gameColor = gamecolorOne;
-
-  if (gameState === 0) {
-    gameColor = gamecolorOne;
+  if (gameReferee === 0) {
+    gameState += gamecolorOne;
   }
-  else if (gameState === 1) {
-    gameColor = gameColorTwo;
+  else if (gameReferee === 1) {
+    gameState += gameColorTwo;
   }
   else {
-    gameColor = gameColorThree;
+    gameState += gameColorThree;
   }
   return (
-    <section className="game">
+    <section className={gameState}>
       <h2 className="game__item">Equipe 1 VS Equipe 2</h2>
       <p className="game__item">01/08/1998</p>
       <p className="game__item">Gymnase du coin de la rue</p>
       <p className="game__item">categorie : U12</p>
-      <p className={gameColor}>{gameState}/2</p>
-      <img className="game__registering" src={registering} alt="registering button" />
+      <p className="game__counter">{gameReferee}/2</p>
+      <a href=""><img className="game__registering" src={registering} alt="registering button" /></a>
     </section>
   );
 }
