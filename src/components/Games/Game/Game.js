e6c6758d0e70b3ddddf2cba90aab2283f6a6d5e7
Runebearer <guegan.l@gmail.com>
2022-09-28 20:22:48 +0200

    code(games): add games data synamisation wip

--- src/components/Games/Game/Game.js
+++ src/components/Games/Game/Game.js
@@ -2,9 +2,12 @@
 import { Link } from 'react-router-dom';
 import '../styles.scss';
 import registering from 'src/assets/img/registering.png';
+import PropTypes from 'prop-types';
 
 // component :
 function Game() {
+  console.log(game.arena);
+  // choose the right color for the view :
   let gameState = 'game';
   const gameReferee = 1;
 
@@ -21,11 +24,14 @@ function Game() {
   else {
     gameState += gameColorThree;
   }
+
+  // using props :
+
   return (
     <section className={gameState}>
       <h2 className="game__item">Equipe 1 VS Equipe 2</h2>
       <p className="game__item">01/08/1998</p>
-      <p className="game__item">Gymnase du coin de la rue</p>
+      <p className="game__item">xxx</p>
       <p className="game__item">categorie : U12</p>
       <p className="game__counter">{gameReferee}/2</p>
       <Link to="/engagement"><img className="game__registering" src={registering} alt="registering button" /></Link>
@@ -33,5 +39,9 @@ function Game() {
   );
 }
 
+// verifying props :
+// Game.propTypes = {
+//   arena: PropTypes.string.isRequired,
+// };
 // export :
 export default Game;
