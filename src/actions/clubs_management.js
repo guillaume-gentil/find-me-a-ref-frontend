f3d7ed6833f421e9a253435ebf2fc1e03b335189
TomasConan <tomasconan.mail@gmail.com>
2022-10-10 15:56:52 +0200

    code(clubsmanagement): create handler, action and methods to delete a club

--- src/actions/clubs_management.js
+++ src/actions/clubs_management.js
@@ -1,6 +1,12 @@
 export const SEND_CLUB_FORM = 'SEND_CLUB_FORM';
+export const DELETE_CLUB = 'DELETE_CLUB';
 
 export const sendClubForm = (formObj) => ({
   type: 'SEND_CLUB_FORM',
   formObj: formObj,
+});
+export const deleteClub = ({ token, id }) => ({
+  type: 'DELETE_CLUB',
+  token: token,
+  id: id,
 });
--- src/middlewares/clubsMiddleware.js
+++ src/middlewares/clubsMiddleware.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { setErrorMessage } from '../actions/ui_actions';
+import { saveClubs } from '../actions/filters';
+import { removeUser } from '../selectors/removeUser';
 
 const clubsMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
@@ -26,6 +28,23 @@ const clubsMiddleware = (store) => (next) => (action) => {
           console.log(error);
           const message = 'Une erreur est survenue.';
           store.dispatch(setErrorMessage(message));
+        });
+      break;
+    case 'DELETE_CLUB':
+      axios.delete(
+        `http://localhost:8000/api/v1/clubs/${action.id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${action.token}`,
+          },
+        },
+      )
+        .then((response) => {
+          const clubs = removeUser(store.getState().clubs, action.id);
+          store.dispatch(saveClubs(clubs));
+        })
+        .catch((error) => {
+          console.log(error);
         });
       break;
     default:
