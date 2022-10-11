2221265bdeea731ceed83ccc9f372c2a28b27755
TomasConan <tomasconan.mail@gmail.com>
2022-10-11 17:02:26 +0200

    code(profile): add logic to profile WIP

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
+import { CornerUpLeft } from 'react-feather';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchUserData } from '../../actions/profile';
 import './styles.scss';
 
@@ -7,6 +9,7 @@ function UserProfile() {
   const dispatch = useDispatch();
   const token = useSelector((state) => state.jwtToken);
   const user = useSelector((state) => state.currentUser);
+  const editState = false;
 
   useEffect(() => {
     dispatch(fetchUserData(token));
@@ -16,6 +19,36 @@ function UserProfile() {
     return (
       <section className="user-profile">
         Hello {`${user.firstname} ${user.lastname}`}
+        {/* TODO faire un truc de ces données */}
+        <div className="user-profile__header">
+          <h2 className="user-profile__title">
+            Profil
+          </h2>
+          <Link to="/">
+            <button
+              type="button"
+              className="user-profile__back"
+            ><CornerUpLeft />
+            </button>
+          </Link>
+        </div>
+        {!editState
+        && (
+        <div className="user-profile__infos">
+          Informations
+        </div>
+        )}
+        {editState
+        && (
+        <div className="user-profile__form">
+          <form action="">
+            <label htmlFor="name">
+              Nom
+              <input type="text" name="name" id="name" />
+            </label>
+          </form>
+        </div>
+        )}
       </section>
     );
   }
