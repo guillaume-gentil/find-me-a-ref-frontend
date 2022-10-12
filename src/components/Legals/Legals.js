593d5e32602d81ae57b8ce080504ccd07aaa0c6d
TomasConan <tomasconan.mail@gmail.com>
2022-10-12 10:24:16 +0200

    chore(legals): add legals

--- src/components/Legals/Legals.js
+++ src/components/Legals/Legals.js
@@ -6,17 +6,17 @@ function Legals() {
   return (
     <div className="informations">
       <section className="legals">
-        <h1 className="legals__title">Mentions Légales</h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet consequatur commodi
-          voluptates iste quidem, esse corporis minus velit laboriosam assumenda excepturi id
-          laborum numquam aliquam ipsum sed blanditiis! Ipsam sapiente, quis officia ipsa earum
-          expedita tempora, qui at rerum voluptatem odio sunt, est nobis dolorum? Sint, maxime!
-          Facilis magnam qui neque optio animi corrupti dolore? Accusantium quisquam esse eum ipsam
-          at rem cumque dolore consequuntur est omnis dicta qae ab eos non nemo fugit hic blanditiis
-          sunt qui optio voluptatem iusto incidunt ration? Excepturi necessitatibus libero voluptate
-          aspernatur sint distinctio esse quaerat quis in itaqe. Nam ad oloribus voluptatem impedit.
-        </p>
+        <h2 className="legals__title">MENTIONS LÉGALES</h2>
+        <p className="legals__text">Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l'économie numérique, il est précisé aux utilisateurs du site Find Me a Ref ! l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi.</p>
+        <h3 className="legals__subtitle">Edition du site</h3>
+        <p className="legals__text">Le présent site, accessible à l’URL www.findmearef/rollerhockey.fr (le « Site »), est édité par :</p>
+        <p className="legals__text">Le Pôle développement de l’arbitrage du roller hockey, ayant son siège situé à 6, Boulevard du Président Franklin Roosevelt – CS11742 – 33080 Bordeaux Cedex France, représentée par Yohann Labreux dûment habilité(e)</p>
+        <h3 className="legals__subtitle">Hébergement</h3>
+        <p className="legals__text">Le Site est hébergé par la société OVH, situé 2 rue Kellermann – 59100 Roubaix – France, (contact téléphonique ou email : +33972101010).</p>
+        <h3 className="legals__subtitle">Nous contacter</h3>
+        <p className="legals__text">Par téléphone : +33556336565</p>
+        <p className="legals__text">Par email : findmearef@gmail.com</p>
+        <p className="legals__text">Par courrier : 6, Boulevard du Pdt Franklin Roosevelt</p>
       </section>
       <section className="partners">
         <h1 className="partners__title">Nos partenaires</h1>
