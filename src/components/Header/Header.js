45decae3280647b74c3c516c5c7712b17a6dbbe4
Runebearer <guegan.l@gmail.com>
2022-11-08 23:29:35 +0100

    chore(header): add Roller Hockey to the title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,8 @@ function Header() {
     <section className="topPartHeader">
       <div className="topPartHeader__headerTitle">
         <h1>Find Me A Ref</h1>
-        <h2>Solution d'arbitrage</h2>
+        <h2 className="topPartHeader__headerTitle--subtitle">Solution d'arbitrage</h2>
+        <h2 className="topPartHeader__headerTitle--subtitle">ROLLER HOCKEY</h2>
       </div>
       <div className="topPartHeader__buttons">
         <Navbar />
